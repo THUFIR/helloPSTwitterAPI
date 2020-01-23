d57ad2ed52e6c4215cf603b1d971a9f11a8c0afe
thufir <hawat.thufir@gmail.com>
2020-01-23 09:03:40 -0800

    incremental

--- twitter.ps1
+++ twitter.ps1
@@ -1,18 +1,12 @@
 Import-Module PSTwitterAPI
-      
 
 Set-TwitterOAuthSettings -ApiKey $env:ApiKey -ApiSecret $env:ApiSecret -AccessToken $env:AccessToken -AccessTokenSecret $env:AccessTokenSecret
-	   
-#Get-TwitterUsers_Lookup -screen_name 'mkellerman'
 
 $TwitterStatuses = Get-TwitterStatuses_UserTimeline -screen_name 'mkellerman'
-#$TwitterStatuses = [array]Get-TwitterStatuses_UserTimeline -screen_name 'mkellerman'
 
 Foreach ($status in $TwitterStatuses) {
    Write-Host $status.text
 }
-
-
 
 Write-Host "done"
 
